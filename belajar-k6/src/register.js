075855e0dbff861846c27c8ebd0c01bbbf7f0d5a
alianhakim9 <alianhakim9@gmail.com>
2024-05-03 08:18:02 +0700

    test multiple request: register, login, get user

--- belajar-k6/src/register.js
+++ belajar-k6/src/register.js
@@ -1,5 +1,5 @@
 import http from "k6/http";
-import { sleep } from "k6";
+// import { sleep } from "k6";
 
 export const options = {
   vus: 10,
@@ -20,4 +20,31 @@ export default function () {
       "Content-Type": "application/json",
     },
   });
+
+  const loginBody = {
+    username: `user-${uniqueId}`,
+    password: "rahasia",
+  };
+
+  const loginResponse = http.post(
+    "http://localhost:3000/api/users/login",
+    JSON.stringify(loginBody),
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const responseBody = loginResponse.json();
+
+  const currentResponse = http.get("http://localhost:3000/api/users/current", {
+    headers: {
+      Accept: "application/json",
+      Authorization: responseBody.data.token,
+    },
+  });
+
+  const currentBody = currentResponse.json();
 }
